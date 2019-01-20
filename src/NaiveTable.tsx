98a8c0c6d874b5fbb5138ba3bab230d290111f9f
untra <untra.sam@gmail.com>
2019-01-20 13:31:09 -0700

    NaiveTable updates, naive render is mostly complete

--- src/NaiveTable.tsx
+++ src/NaiveTable.tsx
@@ -3,8 +3,15 @@ interface TableConfigOptions {
 }
 const defaultOptions: TableConfigOptions = { sizing: "grid" };
 
+interface DataObj {
+  [index: string]: unknown;
+}
+
 interface TableConfigHeader {
+  // this is the string label for the table header
   label: string;
+  // this is the dataKey that is referenced on the data object when displaying this row
+  // if set to '' (empty string) the whole data object will be sent to the render function
   dataKey?: string;
   style?: React.CSSProperties;
   render?: (data: unknown, styles: React.CSSProperties) => JSX.Element;
@@ -13,33 +20,34 @@ const defaultStyle: React.CSSProperties = {
   backgroundColor: "#eee",
   color: "#111"
 };
+const defaultRenderFunc = (data: unknown, styles: React.CSSProperties) => (
+  <p style={styles}>{`${data}`}</p>
+);
 
 const defaultHeaders: TableConfigHeader = {
   dataKey: "",
   label: "",
-  render: (data: unknown, styles: React.CSSProperties) => (
-    <p style={styles}>{`${data}`}</p>
-  ),
+  render: defaultRenderFunc,
   style: defaultStyle
 };
 
 interface TableConfigProps {
   options?: TableConfigOptions;
   headers?: TableConfigHeader[];
-  data: object[];
+  data: DataObj[];
 }
 
 interface TableConfigState {
   options: TableConfigOptions;
   headers: TableConfigHeader[];
-  data: object[];
+  data: DataObj[];
 }
 
 type NaiveTableProps = TableConfigProps;
 
 type NaiveTableState = TableConfigState;
 
-const inferHeadersFromData = (data: object[]): TableConfigHeader[] => {
+const inferHeadersFromData = (data: DataObj[]): TableConfigHeader[] => {
   const paragon = data[0];
   if (paragon) {
     return Object.keys(paragon).map(key => {
@@ -57,11 +65,13 @@ const inferHeadersFromData = (data: object[]): TableConfigHeader[] => {
 class NaiveTable extends React.Component<NaiveTableProps, NaiveTableState> {
   constructor(props: TableConfigProps) {
     super(props);
+    // passed in options shadow the default options
     const options = { ...defaultOptions, ...props.options };
+    // data must be provided. Otherwise if its falsey, it defaults to empty array (no data)
     const data = props.data || [];
+    // if headers are not defined, infer from data keys
     const headers = props.headers ? props.headers : inferHeadersFromData(data);
 
-    // if headers are not defined, infer from data keys
     this.state = {
       options,
       headers,
@@ -73,8 +83,26 @@ class NaiveTable extends React.Component<NaiveTableProps, NaiveTableState> {
     const renderHeader = (header: TableConfigHeader, index: number) => (
       <th key={index}>{header.label}</th>
     );
-    const renderHeaders = <div>{headers.forEach(renderHeader)}</div>;
-    const renderBody = renderHeaders;
+    const renderDataRow = (dataObj: DataObj) => (
+      header: TableConfigHeader,
+      index: number
+    ) => {
+      const render = header.render || defaultRenderFunc;
+      const style = header.style || defaultStyle;
+      const dataVal: any = header.dataKey ? dataObj[header.dataKey] : data;
+      return <td key={index}>{render(dataVal, style)}</td>;
+    };
+
+    const renderDataBody = (tableHeaders: TableConfigHeader[]) => (
+      dataObj: DataObj,
+      indexr: number
+    ) => (
+      <tr key={indexr}>{() => tableHeaders.forEach(renderDataRow(dataObj))}</tr>
+    );
+
+    // todo: sort data if approps
+    const renderHeaders = <tr>{() => headers.forEach(renderHeader)}</tr>;
+    const renderBody = <div>{() => data.forEach(renderDataBody(headers))}</div>;
     return (
       <table>
         {renderHeaders}
