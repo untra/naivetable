a410c676a343eb6f013ddccb23aeb42d2bf5985b
untra <untra.sam@gmail.com>
2019-04-06 15:10:56 -0600

    correct some docs, add some tests

--- site/src/routes/index.tsx
+++ site/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import home from "./home";
 import test from "./test";
--- site/src/routes/test.tsx
+++ site/src/routes/test.tsx
@@ -1,40 +1,45 @@
-import { DataObj, NaiveTable } from "@untra/naivetable";
+import { NaiveTable } from "@untra/naivetable";
 import React from "react";
-const version = "0.0.7"
 
-const blamDataRows = (headers: string[], nToGen: number) => {
-  let rows: DataObj[] = [];
-  for (let index = 0; index < nToGen; index++) {
-    const row: DataObj = {};
-    headers.forEach(header => {
-      row[header] = Math.random();
-    });
-    rows = [...rows, row];
-  }
-  return rows;
-};
+const fooData = [
+    {foo: 1, bar: "2", baz: 3.1, etc: true },
+    {foo: 4, bar: "5", baz: 6.2, etc: false },
+    {foo: 7, bar: "6", baz: 9.3, etc: null },
+    {foo: 10, bar: "11", baz: 12.4, etc: undefined }
+]
 
-const randomData = blamDataRows(["foo", "bar", "baz"], 5);
+const nameData = [
+    { a: 'alex', b: 12, c: 82.56 },
+    { a: 'brock', b: 17, c: 93.33 },
+    { a: 'charlie', b: 16, c: 48.65 }
+]
+
+const nameHeaders = [
+    // change the rendered header text with the 'label' parameter
+    { label: 'name', dataKey: 'a' },
+    // individually style each header cell with the 'style' parameter
+    { label: 'age', dataKey: 'b', style: { backgroundColor: "pink" } },
+    // provide a 'render' function to control how dataCells render for the column
+    { label: 'grade status', dataKey: 'c', render: (val: number) => <h2>{
+        `${val > 50 ? 'passing' : 'failing'} the class`
+    }</h2> },
+    // use the 'dataKey' to control the input to the render function
+    // provide an empty string to instead call render with the entire dataObject provided
+    { label: 'assessment', dataKey: '', render: (val : any) => <h4>{
+        `${val.a} is ${val.c > 90 ? 'really' : ''} ${val.c > 50 ? 'smart' : 'dumb'}`
+    }</h4> },
+    // you can have more headers than keys in your dataObjects, btw ;)
+    // you can also control the 'width' of the column (pass in 'fr' , defaults to 'auto')
+    { label: 'comment', dataKey: '', render: () => 'I like you', width: '4fr' }
+]
+
 export default class Test extends React.Component {
-    public render() {
-      return (
-        <div className="page-content">
-          <div className="wrapper">
-            <h1>🍱 NaiveTable</h1>
-            <h2>dumb simple naive <a href="https://reactjs.org/">React</a> {`Array<T>`} data tables</h2>
-            <span>
-              <h3>v{version} - <a href="https://naivetable.untra.io/docs">Documentation</a> - <a href="https://github.com/untra/naivetable">Github</a> - <a href="https://www.npmjs.com/package/@untra/naivetable">NPM</a></h3>
-            </span>
-            <pre className="highlight">
-            <code>{`
-  const data = [${JSON.stringify(randomData[0])}...]
-  ...
-  <NaiveTable data={data} includeIndex={true} />
-            `}</code>
-            </pre>
-            <NaiveTable data={randomData} includeIndex={true} />
-          </div>
-        </div>
-      );
-    }
+  public render() {
+    return (
+      <div className="wrapper">
+        <NaiveTable data={fooData} />
+        <NaiveTable data={fooData} includeIndex={true} />
+      </div>
+    );
   }
+}
