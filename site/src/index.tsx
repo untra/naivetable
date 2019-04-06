3a2e9db94b0ede85d779a05fcc8f3292dfcbcd18
untra <untra.sam@gmail.com>
2019-04-06 10:41:17 -0600

    update the website, demonstrate naivetable and link to the site

--- site/src/index.tsx
+++ site/src/index.tsx
@@ -1,38 +1,35 @@
-import React from 'react'
-import {
-    render
-} from 'react-dom'
-import NaiveTable from '@untra/naivetable'
+import { DataObj, NaiveTable, NaiveTableProps } from "@untra/naivetable";
+import React from "react";
+import { render } from "react-dom";
 
-const blamDataRows = (headers, nToGen) => {
-    let rows = []
-    for (let index = 0; index < nToGen; index++) {
-        const row = {}
-        headers.forEach(header => {
-            row[header] = Math.random()
-        })
-        rows = [...rows, row]
-    }
-    return rows
-}
+const blamDataRows = (headers: string[], nToGen: number) => {
+  let rows: DataObj[] = [];
+  for (let index = 0; index < nToGen; index++) {
+    const row: DataObj = {};
+    headers.forEach(header => {
+      row[header] = Math.random();
+    });
+    rows = [...rows, row];
+  }
+  return rows;
+};
 
-const bigdata = blamDataRows(['foo', 'bar', 'baz'], 500)
+const bigdata = blamDataRows(["foo", "bar", "baz"], 10);
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
+  constructor(props: NaiveTableProps) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return( <NaiveTable data = {
-            bigdata
-        }
-        includeIndex = {
-            true
-        }
-        />)
-    }
+  public render() {
+    return <div>
+        <h1>NaiveTable</h1>
+        <h2>{`Dumb Simple Naive React Array<T> Table`}</h2>
+        <span>Click to view the <a href="https://naivetable.untra.io/docs">docs</a></span>
+        <NaiveTable data={bigdata} includeIndex={true} />
+    </div>
+  }
 }
 
-render( <App /> , document.getElementById('root'))
+render(<App />, document.getElementById("root"));
