649a54caaa60c9b5c3c081b1ba73abb86614ee06
untra <untra.sam@gmail.com>
2019-04-06 01:32:51 -0600

    stop ignoring all .js assets

---
+++ docs/precache-manifest.69887d7cc99ed3f2e37d23e43de15d24.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae6e3bd6af99d181cbf8",
+    "url": "/static/js/main.7963c8bd.chunk.js"
+  },
+  {
+    "revision": "80951c356fb50b844299",
+    "url": "/static/js/2.bd633252.chunk.js"
+  },
+  {
+    "revision": "3e0df0d60ffdf7069d2042841e5c23b7",
+    "url": "/index.html"
+  }
+];
---
+++ docs/precache-manifest.a0d1a2929b27186ab666e6b55ac784a3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae6e3bd6af99d181cbf8",
+    "url": "/static/js/main.7963c8bd.chunk.js"
+  },
+  {
+    "revision": "80951c356fb50b844299",
+    "url": "/static/js/2.bd633252.chunk.js"
+  },
+  {
+    "revision": "c002f708914b932621403d06ceb31382",
+    "url": "/index.html"
+  }
+];
