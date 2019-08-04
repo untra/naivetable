7c4dd49af7e2182ded428dc46dac11780e8fdec9
untra <untra.sam@gmail.com>
2019-08-04 15:33:58 -0600

    Updates

---
+++ precache-manifest.6fcc97b7cb65ae02f58af5315dc74f70.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2821a2116a3e71813e0cb752a41771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70b7c7577bb6a5ac39e5",
+    "url": "/static/js/2.6e3a7e02.chunk.js"
+  },
+  {
+    "revision": "8c3311ac63dc57ced6c1",
+    "url": "/static/js/main.5052eedf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64c84bcdcb737b3d9e9e73635730ad64.js"
+  "/precache-manifest.6fcc97b7cb65ae02f58af5315dc74f70.js"
 );
 
 self.addEventListener('message', (event) => {
