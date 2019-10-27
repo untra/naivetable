65da1b789749f8869738a31a75bb04451ed8c5e2
untra <sam.volin@stackhawk.com>
2019-10-27 16:23:05 -0600

    fix vulns and publish 0.1.10

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,53 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path');
+
 module.exports = {
+    context: __dirname,
+    entry: './dist',
+    devtool: 'inline-source-map',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json', '.html']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/",
+        filename: 'index.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        stats: {
+            chunks: false
+        }
+    },
+    // node: {
+    //     console: true,
+    //     fs: "empty",
+    //     net: "empty",
+    //     tls: "empty",
+    //     "aws-sdk": "empty"
+    // },
     module: {
         rules: [
             {
-                test: /\.ts$/,
-                exclude: [/node_modules/],
-                use: [
-                    {
-                        loader: 'ts-loader'
-                    }
-                ]
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: [/node_modules/]
             },
             {
+                enforce: "pre",
                 test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
+                exclude: [/node_modules/],
                 use: {
-                    loader: 'babel-loader'
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { loose: true, modules: false }],
+                            '@babel/react'
+                        ],
+                        sourceType: 'unambiguous'
+                    }
                 }
             },
             {
@@ -28,10 +60,12 @@ module.exports = {
             }
         ]
     },
-    // plugins: [
-    //     new HtmlWebPackPlugin({
-    //         template: './src/index.html',
-    //         filename: './index.html'
-    //     })
-    // ]
+    target: 'web',
+    // target: 'node',
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./dist/index.html",
+            filename: "index.html"
+        })
+    ],
 }
